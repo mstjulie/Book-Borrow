438b6b49697dc216dceb57b8d72492f53db9f940
mstjulie <mstjulie612@gamil.com>
2026-07-16 00:44:54 +0600

    fixed error message

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,25 +1,22 @@
 'use client'
-
 import { authClient } from '@/lib/auth-client';
 import Link from 'next/link';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const LoginPage = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const handlerLoginFunc = async (data) => {
-        console.log(data, "data");
-
-        const { data:res, error } = await authClient.signIn.email({
-            email: data.email,
-            password: data.password,
-            rememberMe: true,
-            callbackURL: "/",
-        });
-        console.log(res, error);
-    }
-    
+    const { register, handleSubmit, formState: { errors } } = useForm()
+     const handlerLoginFunc = async (data) => {
+         console.log(data, "data")
+         const { data:res, error } = await authClient.signIn.email({
+             email: data.email,
+             password: data.password,
+             rememberMe: true,
+             callbackURL: "/",
+         });
+         console.log(res, error);
+        }
     return (
         <div className='container mx-auto min-h-[80vh] flex justify-center items-center bg-amber-100 mt-6 mb-6'>
             <div className='px-15 py-12 rounded-xl bg-base-100'>
@@ -34,32 +31,36 @@ const LoginPage = () => {
                             {...register("email")}
                         />
                         {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+
                     </fieldset>
 
                     <fieldset className="fieldset">
-                        <legend className="fieldset-legend">Password</legend>
-                        <input
-                            type="password"
-                            className="input"
-                            placeholder="Type your password"
-                            {...register("password", { required: 'Password file is requird' })}
-                        />
-                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
-                    </fieldset>
-                    <button className="btn w-full bg-[#F39C12] text-[#0B2D5B] text-xl">Login</button>
+                           <legend className="fieldset-legend">Password</legend>
+                         <input
+                             type="password"
+                             className="input"
+                             placeholder="Type your password"
+                             {...register("password", { required: 'Password file is requird' })}
+                         />
+                         {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
 
-                    <h1 className='flex justify-center items-center'>or</h1>
+                    </fieldset>
+                    <button className="btn w-full bg-[#F39C12] text-[#0B2D5B] text-xl mt-3 ">Login</button>
+
                     <div className='flex flex-col gap-4'>
+                        <h1 className='flex justify-center items-center'>or</h1>
                         <button className="btn bg-white text-black border-[#e5e5e5]">
                             <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                             Login with Google
                         </button>
                     </div>
                 </form>
-                <p className='mt-4'>Don't have an account? <Link href={"/register"} className='text-red-500'>Register </Link></p>
+                <p className='mt-4'>
+                    Don't have an account?
+                    <Link href={"/register"} className='text-red-500'>Register </Link></p>
             </div>
         </div>
     );
-};
-
+     
+}
 export default LoginPage;
